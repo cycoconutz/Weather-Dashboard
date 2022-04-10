91099a3b277493ea10ef52b76e549f3aabed6cac
John Yates <johndyates@gmail.com>
2022-04-10 14:19:03 -0500

    forecast working, need to fix layout and search history buttons

--- Assets/script.js
+++ Assets/script.js
@@ -77,34 +77,42 @@ var displayWeather = function (cityWeather) {
 
 var displayForecast = function (cityForecast){
     forecastEl.innerHTML = "";
-    for (i=6; i<=38; i+8){
+
+    for (i=6; i<=38; i=i+8){
         var icon = cityForecast.list[i].weather[0].icon;
         var unixDate = cityForecast.list[i].dt
-        var date = moment.unix(dayOneDate).format('l');
+        var date = moment.unix(unixDate).format('l');
         var temp = "Temp: " + cityForecast.list[i].main.temp + "°"
+        var wind = "Wind: " + cityForecast.list[i].wind.speed + "MPH"
+        var humid = "Humidity: " + cityForecast.list[i].main.humidity + "%";
 
 
+        var card = document.createElement('div');
+        card.classList = 'card text-white bg-dark mb-3 col-2'
+        var cardHead = document.createElement('div');
+        cardHead.classList = "card-header"
+        cardHead.textContent = date;
+        var cardIcon = document.createElement('img');
+        cardIcon.classList = 'media';
+        cardIcon.src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+        var cardTemp = document.createElement('h5');
+        cardTemp.classList = 'card-title';
+        cardTemp.textContent = temp;
+        var cardWind = document.createElement('h5');
+        cardWind.classList = 'card-title';
+        cardWind.textContent = wind;
+        var cardHumid = document.createElement('h5');
+        cardHumid.classList = 'card-title';
+        cardHumid.textContent = humid;
+
+        forecastEl.appendChild(card);
+        card.appendChild(cardHead);
+        cardHead.appendChild(cardIcon);
+        cardHead.appendChild(cardTemp);
+        cardHead.appendChild(cardWind);
+        cardHead.appendChild(cardHumid);
     }
+};
 
-{/* <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Dark card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div> */}
-
-  // var dayTwoDate = cityForecast.list[14].dt
-    // var dateUnixTwo = moment.unix(dayTwoDate).format('l');
-    // var dayThreeDate = cityForecast.list[22].dt
-    // var dateUnixThree = moment.unix(dayThreeDate).format('l');
-    // var dayFourDate = cityForecast.list[30].dt
-    // var dateUnixFour = moment.unix(dayFourDate).format('l');
-    // var dayFiveDate = cityForecast.list[38].dt
-    // var dateUnixFive = moment.unix(dayFiveDate).format('l');
-
-
-}
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
-  
